1704b94f1d10d27d05c81a5eb3a8fd2c324b7cac
AbdouScript <abdoukichenama@gmail.com>
2024-03-24 07:41:48 +0100

    fonction blur pour modeles

---
+++ script.js
@@ -0,0 +1,22 @@
+
+// Fonction pour vérifier si l'élément est dans le viewport
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+  
+  // Écoute l'événement de scroll
+  window.addEventListener('scroll', () => {
+    document.querySelectorAll('.img-model').forEach((img) => {
+      if (isInViewport(img)) {
+        img.classList.add('blurred');
+      } else {
+        img.classList.remove('blurred');
+      }
+    });
+  });
